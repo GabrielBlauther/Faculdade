d4ca5ab69bf5e4dd2723442728b4fa5cdc2a8087
Gabriel Blauther <gblauther@gmail.com>
2025-10-20 20:18:28 +0000

    feat: new exercise

---
+++ Prog1/Prog1_2.0/estudos para prova 2/ex09.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int m,n,ehPrimo,i,j,k,gemeo;
+
+    printf("Digite o intervalo: ");
+    scanf("%d%d",&m,&n);
+    
+    for(i=m;i<n;i++){
+        ehPrimo=1;
+        for(j=2;j<i;j++){
+            if(i%j==0){
+                ehPrimo=0;
+            }
+        }
+        if(ehPrimo==1){
+            gemeo=i+2;
+            for(k=2;k<gemeo;k++){
+                if(gemeo%k==0){
+                    ehPrimo=0;
+                }
+            }
+        }
+        if(ehPrimo==1){
+            printf("%d - %d ",i,gemeo);
+        }
+    }
+}
