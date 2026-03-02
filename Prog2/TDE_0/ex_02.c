8b6dee3359a978497e09d2af2ece734f6b3134e1
Gabriel Blauther <gblauther@gmail.com>
2026-03-02 19:39:29 +0000

    Feat: new exercice.

---
+++ Prog2/TDE_0/ex_02.c
@@ -0,0 +1,27 @@
+/*
+Faça um programa que leia uma matriz M[5][5] e calcule e escreva a soma dos elementos em
+toda a região acima da diagonal principal.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int i, M[5][5], j, soma=0;
+
+     for(i = 0; i < 5; i ++ )
+  {
+    for(j = 0; j < 5; j ++ )
+    {
+      printf("Digite o numero da %dº coluna da %dº linha:  ", i+1, j+1);
+      scanf("%d", &M[i][j]);
+    }
+  }
+  int cont = 1 ;
+  for( i = 0; i < 5; i++){
+    for(j = cont  ; j < 5 ; j++){
+      soma += M[i][j];
+    }
+    cont++;
+  }
+  printf("%d",soma);
+}
